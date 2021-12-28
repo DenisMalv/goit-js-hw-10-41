73b19f06631097e7ffeb7c9c19e7fb6cb627426b
DenisMalv <kissed03@gmail.com>
2021-12-29 01:58:33 +0200

    update2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,4 @@
 function fetchCountries(name) {
-    console.log(name)
    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
         if (!response.ok) {
@@ -7,12 +6,8 @@ function fetchCountries(name) {
         }
         return response.json();
     })
-    .then((responce) => {
-        // console.log(responce)
-        return responce
-    })
     .catch(error => {
-         console.log('Это error: ',error)
+         console.log('Это Error: ',error)
     });
 }
 export { fetchCountries }
--- src/index.js
+++ src/index.js
@@ -13,35 +13,30 @@ const refs = {
     countryInfo: document.querySelector('.country-info')
 }
 const { input, countryList, countryInfo } = refs
-console.log(countryList)
-console.log(countryInfo)
-input.addEventListener('input', debounce(onInputChangeValue, 300))
+
+input.addEventListener('input', debounce(onInputChangeValue, DEBOUNCE_DELAY))
 
 function onInputChangeValue(event) {
-    console.log(event.target.value.trim())
-    const name = event.target.value.trim()
+console.log(event.target.value.trim())
+clearMarkup()
+const name = event.target.value.trim()
     if (name.length >= 1) {
         fetchCountries(name)
-            .then((responce) => {
-                // console.log(responce)
-                console.log(responce.length)
-                if (responce.length > 10) {
-                    clearMarkup()
-                    return Notify.info("Too many matches found. Please enter a more specific name.");
-                }
-                if (responce.length !== 1) {
-                    clearMarkup()
-                    countryArrayMarkup(responce)
-                }
-                if (responce.length === 1) {
-                    clearMarkup()
-                    countryInfoMarkup(responce)
-                }
-            })
-            .catch((error) => {
-                clearMarkup()
-                Notify.failure("Oops, there is no country with that name");
-            });
+        .then((responce) => {
+            if (responce.length > 10) {
+                return Notify.info("Too many matches found.Please enter a more specific name.");
+            }
+            if (responce.length !== 1) {
+                countryArrayMarkup(responce)
+            }
+            if (responce.length === 1) {
+                countryInfoMarkup(responce)
+            }
+        })
+        .catch((error) => {
+            clearMarkup()
+            Notify.failure("Oops, there is no country with that name");
+        });
     }
     
 }
@@ -51,28 +46,24 @@ function clearMarkup() {
 }
 
 function countryInfoMarkup(object) {
-    console.log(object)
+
     const infoMarkup = object.map(({ name, flags, capital, population, languages }) => {
-        console.log(name.official)
-        console.log(flags)
-        console.log(capital)
-        console.log(population)
-        console.log(languages)
-        console.log(Object.values(languages))
+
         const eachLanguage = Object.values(languages).map((el)=>el).join(", ")
-    return `
-    <p class="country_name"><img style="width:30px; margin-right:20px" class="country_flag" src="${flags.svg}" alt="${name.official}">${name.official}</p>
-    <p class="country_capital">Capital: ${capital}</p>
-    <p class="country_population">Population: ${population}</p>
-    <p class="country_languages">Languages: ${eachLanguage}</p>`
+        return `<p class="country_name"><img style="width:30px; margin-right:20px" class="country_flag" src="${flags.svg}" alt="${name.official}">${name.official}</p>
+        <p class="country_capital">Capital: ${capital}</p>
+        <p class="country_population">Population: ${population}</p>
+        <p class="country_languages">Languages: ${eachLanguage}</p>`
     }).join('')
+    
     countryInfo.insertAdjacentHTML('beforeend', infoMarkup)
 }
 
 function countryArrayMarkup(array) {
-    const arrayMarkup = array.map(({ name, flags }) => {
-        console.log(array)
+    const arrayMarkup = array.map(({ name, flags }) =>
+    {
         return `<li style="margin:10px;" ><img style="width:30px; margin-right:20px" src="${flags.svg}" alt="${name.common?name.common:name.official}"><span>${name.common?name.common:name.official}</span></li>`
     }).join("")
+
     countryList.insertAdjacentHTML('beforeend',arrayMarkup)
 }
